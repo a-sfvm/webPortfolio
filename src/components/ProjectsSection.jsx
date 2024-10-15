8dbbd9de5764d75f977e15a3d89be909f5db38b9
Afonso Melo <afonsomelo27@gmail.com>
2024-10-15 22:27:35 +0100

    images

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,6 +1,14 @@
+import jpeeg from "../assets/SurfRnR-home.jpg"
+import classes from "./ProjectsSection.module.css"
+
 function ProjectsSection() {
   return (
-    <h1>Some text</h1>
+    <>
+      <h1>Some text</h1>
+      <div className={classes.imagecont}>
+      <img src={jpeeg} alt="" className={classes.image}/>
+      </div>
+    </>
   )
 }
 
